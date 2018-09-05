233c6051851e31f315ad2883f4a10a49c8ad1550
Hossein Sadeghi <ho.mysterious@gmail.com>
2018-09-05 13:16:11 +0430

    Create lazy di callable

---
+++ src/Skyroom/Util/DICallable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Skyroom\Util;
+
+use DI\Container;
+
+class DICallable
+{
+    /**
+     * @var Container $container
+     */
+    private $container;
+
+    /**
+     * @var Callable $callable
+     */
+    private $callable;
+
+    /**
+     * DICallable constructor.
+     *
+     * @param Container $container Container object
+     * @param Callable  $callable  Function to call
+     */
+    public function __construct(Container $container, $callable)
+    {
+        $this->container = $container;
+        $this->callable = $callable;
+    }
+
+    public function __invoke(...$params)
+    {
+        try {
+            $reflection = is_array($this->callable)
+                ? new \ReflectionMethod($this->callable[0], $this->callable[1])
+                : new \ReflectionFunction($this->callable);
+
+            $parameters = [];
+            $params = $reflection->getParameters();
+            $args = func_get_args();
+            for ($i = 0; $i < func_num_args(); $i++) {
+                $name = $params[$i]->name;
+                $parameters[$name] = $args[$i];
+            }
+
+            $this->container->call($this->callable, $parameters);
+        } catch (\ReflectionException $e) {
+        }
+    }
+}
