96f4353d051503ade17be8ccf0c56da831122739
Hossein Sadeghi <ho.mysterious@gmail.com>
2018-10-29 13:27:55 +0330

    Add method for finding unsynced users

--- src/Skyroom/Repository/UserRepository.php
+++ src/Skyroom/Repository/UserRepository.php
@@ -67,6 +67,19 @@ class UserRepository
     }
 
     /**
+     * Get users that are not synced with server
+     *
+     * @return \WP_User[]
+     */
+    public function getUnsyncedWPUsers()
+    {
+        return get_users([
+            'meta_key' => self::SKYROOM_ID_META_KEY,
+            'meta_compare' => 'NOT EXISTS',
+        ]);
+    }
+
+    /**
      * Add registered user to skyroom
      *
      * @throws ConnectionNotEstablishedException
